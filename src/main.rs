7be7238e043fbde1f9c87dc8d3925256bc78eeef
Benjamin Kampmann <ben@create-build-execute.com>
2016-07-25 21:36:30 +0200

    Nickel Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use] extern crate nickel;
+
+use nickel::{Nickel, HttpRouter};
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello world!"
+        }
+    });
+    server.listen("127.0.0.1:6767");
 }
