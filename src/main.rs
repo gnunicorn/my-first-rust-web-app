8803b09a00c74375a4811721645ff3b6a04db280
Benjamin Kampmann <ben@create-build-execute.com>
2016-07-25 21:53:37 +0200

    Really converting stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 #[macro_use] extern crate nickel;
 
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, HttpRouter, QueryString};
+use std::str::FromStr;
 
 fn main() {
     let mut server = Nickel::new();
-    server.utilize(router! {
-        get "**" => |_req, _res| {
-            "Hello world!"
+    server.get("/convert/meter2feet", middleware! { |request|
+        if let Some(meter) = request.query().get("meter") {
+            match f32::from_str(meter) {
+                Ok(val) => format!("{:?}", val * 3.2808),
+                Err(_) => format!("Couldn't parse 'meter'")
+            }
+        } else {
+            format!("You have to supply 'meter' parameter")
         }
     });
     server.listen("127.0.0.1:6767");
