dc7bdcfb59b9aeebc2cd37083000c4fc22b97e90
Benjamin Kampmann <ben@create-build-execute.com>
2016-07-26 23:03:56 +0200

    Add meter2feet function

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,24 @@ extern crate iron;
 use iron::prelude::*;
 use iron::status;
 
+#[macro_use]
+extern crate router;
+
+use router::Router;
+use std::str::FromStr;
+
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
+    fn meter2feet(req: &mut Request) -> IronResult<Response> {
+
+        let meter = req.extensions.get::<Router>().unwrap().find("meter").unwrap();
+        match f32::from_str(meter) {
+                Ok(val) => Ok(Response::with((status::Ok, format!("{:?}", val * 3.2808)))),
+                Err(_) => Ok(Response::with((status::BadRequest, format!("Couldn't parse 'meter'"))))
+        }
     }
 
     println!("On 6767");
-    Iron::new(hello_world).http("localhost:6767").unwrap();
+    Iron::new(router!(
+        get "/convert/meter2feet/:meter" => meter2feet
+    )).http("localhost:6767").unwrap();
 }
