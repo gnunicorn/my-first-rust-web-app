8c97abb7d280f87635889e2e6e28de46d053a099
Benjamin Kampmann <ben@create-build-execute.com>
2016-07-26 22:49:00 +0200

    Iron hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello World!")))
+    }
+
+    println!("On 6767");
+    Iron::new(hello_world).http("localhost:6767").unwrap();
 }
