68894b99c5d7742cf35109ce82cafbdaa7742f4e
Benjamin Kampmann <ben@create-build-execute.com>
2016-07-27 13:25:35 +0200

    adding templating

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,61 @@
 
 extern crate iron;
-
-use iron::prelude::*;
-use iron::status;
+extern crate handlebars_iron as hbs;
+extern crate urlencoded;
 
 #[macro_use]
 extern crate router;
 
+use iron::prelude::*;
+use iron::status;
+
 use router::Router;
 use std::str::FromStr;
 
-fn main() {
-    fn meter2feet(req: &mut Request) -> IronResult<Response> {
+use hbs::{Template, HandlebarsEngine, DirectorySource};
+use urlencoded::UrlEncodedQuery;
+use std::collections::HashMap;
 
-        let meter = req.extensions.get::<Router>().unwrap().find("meter").unwrap();
-        match f32::from_str(meter) {
-                Ok(val) => Ok(Response::with((status::Ok, format!("{:?}", val * 3.2808)))),
-                Err(_) => Ok(Response::with((status::BadRequest, format!("Couldn't parse 'meter'"))))
+fn meter2feet(req: &mut Request) -> IronResult<Response> {
+
+    let mut resp = Response::new();
+    let mut data = HashMap::new();
+
+    if let Ok(ref map) = req.get_ref::<UrlEncodedQuery>() {
+        if let Some(ref vals) = map.get("meter") {
+            if let Some(val) = vals.first() {
+                if let Ok(meter) = f32::from_str(val) {
+                    data.insert("meter".to_string(), meter.to_string());
+                    data.insert("feet".to_string(), (meter * 3.2808).to_string());
+                }
+            }
         }
     }
 
-    println!("On 6767");
-    Iron::new(router!(
-        get "/convert/meter2feet/:meter" => meter2feet
-    )).http("localhost:6767").unwrap();
+    resp.set_mut(Template::new("meter2feet", data)).set_mut(status::Ok);
+    Ok(resp)
+}
+
+fn main() {
+
+    let mut hbse = HandlebarsEngine::new();
+    hbse.add(Box::new(DirectorySource::new("./templates/", ".hbs")));
+
+    // load templates from all registered sources
+    if let Err(r) = hbse.reload() {
+      panic!("{}", r);
+    }
+
+
+    let mut router = Router::new();
+    router.get("/convert/meter2feet", meter2feet);
+
+    // make chainable
+    let mut chain = Chain::new(router);
+    // add templates
+    chain.link_after(hbse);
+
+
+    println!("Server running at http://localhost:6767/");
+    Iron::new(chain).http("localhost:6767").unwrap();
 }
